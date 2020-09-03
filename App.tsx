83b9c7fcb597e3f652b23e5ce7233ca387ece38a
LucasGobatto <lucas.bisaio@live.com>
2020-09-03 15:43:16 -0300

    arrumando as cagadas

--- App.tsx
+++ App.tsx
@@ -1,80 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  TextInput,
-  Button,
-  Alert,
-} from 'react-native';
-
-declare const global: {HermesInternal: null | {}};
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, TextInput, Button, Alert} from 'react-native';
 
 const App = () => {
+  const [email, setEmail] = useState(' ');
+  const [password, setPassword] = useState(' ');
+
+  function inputValue() {
+    if (!passwordTest() || !emailTest()) {
+      Alert.alert('Incorrect email or password format');
+    } else {
+      Alert.alert("it's all ok");
+    }
+  }
+
+  function passwordTest() {
+    const regex = /(([a-z]+[A-Z]+|[A-Z]+[a-z]+)|([0-9]+[A-Za-z]+)|([a-zA-Z]+[0-9])+|([\W]))/;
+    return password.length >= 7 && regex.test(password) ? true : false;
+  }
+  function emailTest() {
+    return email.indexOf('@') && email.indexOf('.com') !== -1 ? true : false;
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.title}>
-          <Text style={styles.text}>Bem vindo(a) à Taqtile!</Text>
-        </View>
-        <View style={styles.container}>
-          <View style={styles.inputContainer}>
-            <Text style={styles.descriptionText}>E-mail</Text>
-            <TextInput style={styles.input}> </TextInput>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.descriptionText}> Senha</Text>
-            <TextInput style={styles.input}> </TextInput>
-          </View>
-          <Button onPress={() => Alert.alert('pressed')} title="Entrar" />
-        </View>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem vindo(a) à Taqtile!</Text>
+      <View style={styles.viewLogin}>
+        <Text style={styles.textLogin}>E-mail</Text>
+        <TextInput
+          style={styles.inputLogin}
+          onChangeText={(text) => setEmail(text)}>
+          {email}
+        </TextInput>
+        <Text style={styles.textLogin}>Senha</Text>
+        <TextInput
+          style={styles.inputLogin}
+          onChangeText={(text) => setPassword(text)}>
+          {password}
+        </TextInput>
+      </View>
+      <Button onPress={inputValue} title="Entrar" />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
 
   container: {
-    alignItems: 'center',
-    padding: 30,
+    paddingTop: 80,
+    justifyContent: 'center',
   },
   title: {
-    backgroundColor: 'white',
-    paddingTop: 100,
-  },
-  text: {
-    textAlign: 'center',
     fontSize: 50,
-  },
-  inputContainer: {
-    padding: 10,
-  },
-  input: {
     textAlign: 'center',
-    width: 350,
-    height: 70,
-    fontSize: 24,
-    borderWidth: 3,
-    borderRadius: 25,
-    borderColor: '#a6a6a6',
+    paddingBottom: 40,
   },
-  descriptionText: {
+  viewLogin: {
+    alignItems: 'flex-start',
+    marginHorizontal: 40,
+  },
+  textLogin: {
+    fontSize: 24,
+    paddingTop: 20,
+  },
+  inputLogin: {
+    textAlign: 'center',
     fontSize: 20,
-    padding: 9,
+    width: 340,
+    height: 50,
+    borderWidth: 5,
+    borderRadius: 25,
+    borderColor: '#CCCC',
   },
 });
 
