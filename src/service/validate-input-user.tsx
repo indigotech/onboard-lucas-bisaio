5731c91360462dae92139a6c773dbc1b896d192c
LucasGobatto <lucas.bisaio@live.com>
2020-09-11 11:25:29 -0300

    review - validate-input-user file

--- src/service/validate-input-user.tsx
+++ src/service/validate-input-user.tsx
@@ -1,9 +1,9 @@
 import {Alert} from 'react-native';
 
 export function validateName(name: string): boolean {
-  const nameValidation = /[0-9]|\W|_/;
+  const nameValidation = /[0-9]/;
   if (name.length === 0 || nameValidation.test(name)) {
-    var error = `Incorrect input. "${name}" is not a valid name. Try again.`;
+    const error = `Incorrect input. "${name}" is not a valid name. Try again.`;
     Alert.alert(error);
     return false;
   } else {
@@ -12,7 +12,7 @@ export function validateName(name: string): boolean {
 }
 
 export function validateEmail(email: string): boolean {
-  var error = `Incorrect input. Email "${email}" is not valid. Try again.`;
+  const error = `Incorrect input. Email "${email}" is not valid. Try again.`;
   if (email.indexOf('@') && email.indexOf('.com') === -1) {
     Alert.alert(error);
     return false;
@@ -23,7 +23,7 @@ export function validateEmail(email: string): boolean {
 
 export function validatePhone(phone: string): boolean {
   const phoneValidation = /[a-z]|[A-Z]\W|_/;
-  var error = `Incorrect input. Phone "${phone}" is not valid. The correct format is 99999999 Try again.`;
+  const error = `Incorrect input. Phone "${phone}" is not valid. The correct format is 99999999 Try again.`;
   if (phoneValidation.test(phone)) {
     Alert.alert(error);
     return false;
@@ -35,16 +35,15 @@ export function validatePhone(phone: string): boolean {
 export function validateBirthDate(birthDate: string): boolean {
   const birthDateValidation = /(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))/;
   if (!birthDateValidation.test(birthDate)) {
-    var error = `Invalid format. Birth date "${birthDate}" is not valid. The correct format is DD-MM-YYYY. Try again`;
+    const error = `Invalid format. Birth date "${birthDate}" is not valid. The correct format is YYYY-MM-DD. Try again`;
     Alert.alert(error);
     return false;
   } else {
     const parts: string[] = birthDate.split('-');
     const today = new Date();
     const date = new Date(parts[0], parts[1] - 1, parts[2]);
-    console.log(date, parts, date > today, birthDateValidation.test(birthDate));
     if (date >= today) {
-      var error = `Invalid input. Date "${birthDate}" is not valid. It has to be a past date. Try again`;
+      const error = `Invalid input. Date "${birthDate}" is not valid. It has to be a past date. Try again`;
       Alert.alert(error);
       return false;
     } else {
@@ -55,8 +54,7 @@ export function validateBirthDate(birthDate: string): boolean {
 
 export function validatePassword(password: string): boolean {
   const passwordValidation = /(?=.{7,})(?=.*[0-9])(?=.*[a-z])|(?=.{7,})(?=.*[0-9])(?=.*[A-Z])/;
-  var error =
-    'Invalid input. Password is not valid. It have to containe letters and numbers. Try again';
+  const error = 'Typo no containe. Try again';
   if (passwordValidation.test(password)) {
     return true;
   } else {
